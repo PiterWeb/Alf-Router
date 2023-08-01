e7f0eab3285166f2962f993e01b6ef4af7df5d88
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-08-01 10:30:50 +0200

    using mutex to access routes map in go routines & refactor

--- routes.go
+++ routes.go
@@ -1,73 +1,91 @@
 package alf
 
 import (
-	// "fmt"
+	"sync"
+
+	misc "github.com/PiterWeb/Alf-Router/errors"
 	"github.com/pterm/pterm"
 	"github.com/pterm/pterm/putils"
 	"github.com/valyala/fasthttp"
-	"sync"
-	// "time"
 )
 
 var wg sync.WaitGroup
 
-func createRoute(r finalRoute) finalRoute { // create the route with the given parameters
+const initialPath string = ""
+
+func (r *routes) addRoute(method string, path string, route *finalRoute) {
+
+	r.mu.Lock()
+
+	r.methodRoutes[method][path] = createRoute(&finalRoute{
+		Method:     route.Method,
+		Handle:     route.Handle,
+		Middleware: route.Middleware,
+		Headers:    route.Headers,
+		Error:      route.Error,
+	})
+	r.mu.Unlock()
+
+}
+
+func createRoute(r *finalRoute) finalRoute { // create the route with the given parameters
 
 	if r.Error == nil {
 
-		r.Error = func(ctx *fasthttp.RequestCtx, err error) {
+		r.Error = func(ctx *Ctx, err error) {
 			ctx.WriteString("Route Error: " + err.Error())
 			ctx.SetStatusCode(fasthttp.StatusInternalServerError)
 		}
 
 	}
 
-	return r
+	return *r
 
 }
 
-func createChildrenRoutes(routes methodRoutes, r Route, initialPath string) {
+func routeIsNotRootAndHaveChildren(r Route) bool {
 
-	if r.Path != "/" && (r.Children != nil && len(r.Children) > 0) { // if the route is not the root route and has children
+	return r.Path != "/" && (r.Children != nil && len(r.Children) > 0)
+}
 
-		initialPath = initialPath + r.Path
+func createChildrenRoutes(routes *routes, r Route, initialPath string) {
 
-		for _, child := range r.Children {
+	if !routeIsNotRootAndHaveChildren(r) {
+		return
+	}
 
-			// start := time.Now()
+	initialPath = initialPath + r.Path
 
-			if !child.Method.valid() {
-				showError("Invalid method ( " + child.Method.string() + " ) on route " + initialPath + child.Path)
-				continue
-			}
+	for _, child := range r.Children {
 
-			if child.Path == "" || child.Path == "/" {
-				showInternalError("Invalid path set on route: ( " + initialPath + child.Path + " )")
-				continue
-			}
+		if !child.Method.valid() {
+			misc.ShowError("Invalid method ( " + child.Method.string() + " ) on route " + initialPath + child.Path)
+			continue
+		}
 
-			wg.Add(1)
+		if child.Path == "" || child.Path == "/" {
+			misc.ShowInternalError("Invalid path set on route: ( " + initialPath + child.Path + " )")
+			continue
+		}
 
-			go func() {
+		wg.Add(1)
 
-				routes[r.Method.string()][initialPath+child.Path] = createRoute(finalRoute{ // generate new subroute
-					Method:     child.Method,
-					Handle:     child.Handle,
-					Middleware: append(r.Middleware, child.Middleware...), // apply middlewares of the parent route
-					Headers:    append(r.Headers, child.Headers...),       // apply headers of the parent route
-					Error:      child.Error,
-				})
+		go func() {
 
-				wg.Done()
+			routes.addRoute(r.Method.string(), initialPath+child.Path, &finalRoute{ // generate new subroute
+				Method:     child.Method,
+				Handle:     child.Handle,
+				Middleware: append(r.Middleware, child.Middleware...), // apply middlewares of the parent route
+				Headers:    append(r.Headers, child.Headers...),       // apply headers of the parent route
+				Error:      child.Error,
+			})
 
-			}()
+			wg.Done()
 
-			if child.Children != nil && len(child.Children) > 0 {
-				createChildrenRoutes(routes, child, initialPath)
-			}
+		}()
 
-			// fmt.Printf("Child Route: (%s) => %.10d\n", initialPath+child.Path, time.Since(start).Nanoseconds())
-
+		if child.Children != nil && len(child.Children) > 0 {
+			createChildrenRoutes(routes, child, initialPath)
 		}
 
 	}
@@ -78,32 +96,34 @@ func CreateRouter(r []Route) methodRoutes { // creates the routes of the app
 
 	pterm.DefaultBigText.WithLetters(putils.LettersFromStringWithStyle("ALF", pterm.NewStyle(pterm.FgBlue))).Render()
 
-	routes := methodRoutes{
-		"GET":    make(map[string]finalRoute),
-		"POST":   make(map[string]finalRoute),
-		"DELETE": make(map[string]finalRoute),
-		"PUT":    make(map[string]finalRoute),
+	routes := routes{
+		methodRoutes: methodRoutes{
+			"GET":    make(map[string]finalRoute),
+			"POST":   make(map[string]finalRoute),
+			"DELETE": make(map[string]finalRoute),
+			"PUT":    make(map[string]finalRoute),
+		},
 	}
 
 	for _, route := range r {
 
 		if !route.Method.valid() {
-			showError("Invalid method ( " + route.Method.string() + " ) on route " + route.Path)
+			misc.ShowError("Invalid method ( " + route.Method.string() + " ) on route " + route.Path)
 			continue
 		}
 
 		if route.Path == "" {
-			showInternalError("Invalid path set on route: (" + route.Path + " )")
+			misc.ShowInternalError("Invalid path set on route: (" + route.Path + " )")
 			continue
 		}
 
-		createChildrenRoutes(routes, route, "")
+		createChildrenRoutes(&routes, route, initialPath)
 
-		// start := time.Now()
+		wg.Add(1)
 
 		go func(route Route) {
 
-			routes[route.Method.string()][route.Path] = createRoute(finalRoute{
+			routes.addRoute(route.Method.string(), route.Path, &finalRoute{
 				Method:     route.Method,
 				Handle:     route.Handle,
 				Middleware: route.Middleware,
@@ -111,13 +131,13 @@ func CreateRouter(r []Route) methodRoutes { // creates the routes of the app
 				Error:      route.Error,
 			})
 
-			// fmt.Printf("Normal Route: (%s) => %.10d\n", route.Path, time.Since(start).Nanoseconds())
+			wg.Done()
 
 		}(route)
 
 		wg.Wait()
 	}
 
-	return routes
+	return routes.methodRoutes
 
 }
--- types.go
+++ types.go
@@ -1,10 +1,12 @@
 package alf
 
 import (
+	"sync"
+
 	"github.com/valyala/fasthttp"
 )
 
-type Ctx = fasthttp.RequestCtx // alias for fasthttp.RequestCtx
+type Ctx = fasthttp.RequestCtx // alias for Ctx
 
 type Method string // GET, POST, PUT, DELETE
 
@@ -16,31 +18,35 @@ type Header struct {
 type Middleware func(ctx *Ctx) bool // func that returns true if passed and false if an error ocurred
 
 type Route struct {
-	Path       string                         // path
-	Method     Method                         // method (only one)
-	Handle     func(ctx *fasthttp.RequestCtx) error // func that handles the route
-	Children   []Route                        // children of the route (if the route isnt the root route) [all childrens inherit parents Middlewares and Headers ]
-	Error      func(ctx *fasthttp.RequestCtx, err error) // func that handles errors of the route (if err returned on handle method will be invoked with the error in the parameters)
-	Middleware []Middleware                   // middlewares of the route
-	Headers    []Header                       // headers
+	Path       string                    // path
+	Method     Method                    // method (only one)
+	Handle     func(ctx *Ctx) error      // func that handles the route
+	Children   []Route                   // children of the route (if the route isnt the root route) [all childrens inherit parents Middlewares and Headers ]
+	Error      func(ctx *Ctx, err error) // func that handles errors of the route (if err returned on handle method will be invoked with the error in the parameters)
+	Middleware []Middleware              // middlewares of the route
+	Headers    []Header                  // headers
 }
 
 type finalRoute struct {
 	Method     Method
-	Handle     func(ctx *fasthttp.RequestCtx) error
+	Handle     func(ctx *Ctx) error
 	Middleware []Middleware
 	Headers    []Header
-	Error      func(ctx *fasthttp.RequestCtx, err error)
+	Error      func(ctx *Ctx, err error)
 }
 
 type AppConfig struct {
-	Routes      methodRoutes                   // routes of the app
-	Middleware  []Middleware                   // global middlewares
-	Headers     []Header                       // global headers
-	Port        string                         // port of the app
-	NotFound    func(ctx *fasthttp.RequestCtx) // func that handles NotFound requests
-	NotAllowed  func(ctx *fasthttp.RequestCtx) // func that handles requests of paths without the method allowed
-	ServeStatic bool                           // if true, the app will serve static files on "/static"
+	Routes      methodRoutes   // routes of the app
+	Middleware  []Middleware   // global middlewares
+	Headers     []Header       // global headers
+	Port        string         // port of the app | default value '8080'
+	NotFound    func(ctx *Ctx) // func that handles NotFound requests
+	ServeStatic bool           // if true, the app will serve static files on "/static"
+}
+
+type routes struct {
+	mu sync.Mutex
+	methodRoutes
 }
 
 type methodRoutes map[string]map[string]finalRoute // 1º string = method ("GET", ...) ; 2º string = path ("/api", ...) ;
