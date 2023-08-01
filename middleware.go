4b4649815a8b0f830dce5159550dd1d66ff4f1dc
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-08-01 10:30:08 +0200

    Ctx local type used

--- middleware.go
+++ middleware.go
@@ -1,10 +1,8 @@
 package alf
 
-import "github.com/valyala/fasthttp"
+func handleMiddleware(ctx *Ctx, m *[]Middleware, next *bool) {
 
-func handleMiddleware(ctx *fasthttp.RequestCtx, m []Middleware, next *bool) {
-
-	for _, middleware := range m {
+	for _, middleware := range *m {
 		*next = middleware(ctx) // if middleware returns false, it will stop the execution of the route
 		if !*next {
 			break
