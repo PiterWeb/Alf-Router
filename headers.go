f004ea68559eac3f72bd5a48bb93ed6196374faf
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-08-01 10:29:57 +0200

    Ctx local type used

--- headers.go
+++ headers.go
@@ -1,10 +1,8 @@
 package alf
 
-import "github.com/valyala/fasthttp"
+func handleHeaders(ctx *Ctx, h *[]Header) {
 
-func handleHeaders(ctx *fasthttp.RequestCtx, h []Header) {
-
-	for _, header := range h {
+	for _, header := range *h {
 		ctx.Response.Header.Set(header.Name, header.Value) // write global headers
 	}
 
