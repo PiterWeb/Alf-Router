7b0003566f1424f7c13cd0b055d77cbdf251db8a
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-08-05 01:27:28 +0200

    TS-plugin automatically search for the default module

--- plugins/ts/ts-plugin.go
+++ plugins/ts/ts-plugin.go
@@ -1,7 +1,10 @@
 package ts
 
 import (
+	"os"
+
 	"github.com/gzuidhof/tygo/tygo"
+	"golang.org/x/mod/modfile"
 )
 
 type TS_config struct {
@@ -11,8 +14,24 @@ type TS_config struct {
 
 func Init_ts(ts_config TS_config) error {
 
+	cwd, err := os.Getwd()
+
+	if err != nil {
+		return err
+	}
+
+	modFile, err := os.ReadFile(cwd + "/go.mod")
+
+	if err != nil {
+		return err
+	}
+
+	mainPkg := modfile.ModulePath(modFile)
+
 	config := &tygo.Config{
-		Packages: []*tygo.PackageConfig{},
+		Packages: []*tygo.PackageConfig{
+			{Path: mainPkg, OutputPath: ts_config.OutputFolder + "/main.ts"},
+		},
 	}
 
 	for _, pkg := range ts_config.Packages {
