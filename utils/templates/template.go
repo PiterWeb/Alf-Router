0a0e62e10856030b26ac4a07dd9055706a2ca52b
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-08-05 18:55:15 +0200

    Templates Out of the Box

---
+++ utils/templates/template.go
@@ -0,0 +1,18 @@
+package templates
+
+import (
+	"html/template"
+
+	alf "github.com/PiterWeb/Alf-Router"
+)
+
+func Templates(templatesFolder string) func(ctx *alf.Ctx, templateName string, data any) error {
+
+	tpl := template.Must(template.ParseGlob(templatesFolder + "/*.go.html"))
+
+	return func(ctx *alf.Ctx, templateName string, data any) error {
+		ctx.SetContentType("text/html")
+		return tpl.ExecuteTemplate(ctx, templateName, data)
+	}
+
+}
